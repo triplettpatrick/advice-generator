8e80a449fef56f55b21f56c358c67fb4e841684b
triplettpatrick <patrick_triplett@alumni.brown.edu>
2023-03-03 16:13:38 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const endpoint = "https://api.adviceslip.com/advice";
+
+const advice = document.getElementById("advice");
+const button = document.querySelector("a");
+
+function getAdvice() {
+    return fetch(endpoint)
+      .then(response => response.json())
+      .then(data => {
+        return {
+            advice: data.slip.advice,
+            id: data.slip.id
+        }
+        
+      });
+};
+
+const quote = button.addEventListener("click", function() {
+    const promise = new Promise((resolve) => {
+        getAdvice().then(advice => {
+            resolve(advice);
+            // console.log(advice);
+        })
+    })
+
+    promise.then(result => {
+        // console.log(result.id);
+        advice.innerHTML = result.advice;
+        if (advice.innerHTML.length > 99) {
+            advice.style.fontSize = '1.3em';
+        }
+
+        const number = document.getElementById("number");
+        number.innerHTML = '#' + result.id;
+    })
+
+});
+
+
+
+
+
+
+        
+
+
